e707100e428ff6f47d88b952f513ba3621142f3e
Balajinaidu007 <balaji.suvvada@skyronsoftware.com>
2026-03-17 16:30:54 +0530

    Adding JSON

--- src/main/java/com/vertex/ExportRequest.java
+++ src/main/java/com/vertex/ExportRequest.java
@@ -2,30 +2,34 @@ package com.vertex;
 import java.util.List;
 
 public class ExportRequest {
-    private Data Data;
+    private Data data;
 
-    public Data getData() { return Data; }
-    public void setData(Data data) { Data = data; }
+    public Data getData() { return data; }
+    public void setData(Data data) { this.data = data; }
 }
 
 class Data {
-    private Root Root;
-    private List<String> TotalIds;
-    private List<String> OptionalIds;
+    private Root root;
+    private List<String> totalIds;
+    private List<String> optionalIds;
 
-    public Root getRoot() { return Root; }
-    public void setRoot(Root root) { Root = root; }
+    public Root getRoot() { return root; }
+    public void setRoot(Root root) { this.root = root; }
 
-    public List<String> getTotalIds() { return TotalIds; }
-    public void setTotalIds(List<String> totalIds) { TotalIds = totalIds; }
+    public List<String> getTotalIds() { return totalIds; }
+    public void setTotalIds(List<String> totalIds) { this.totalIds = totalIds; }
 
-    public List<String> getOptionalIds() { return OptionalIds; }
-    public void setOptionalIds(List<String> optionalIds) { OptionalIds = optionalIds; }
+    public List<String> getOptionalIds() { return optionalIds; }
+    public void setOptionalIds(List<String> optionalIds) { this.optionalIds = optionalIds; }
 }
 
 class Root {
     private String id;
+    private String type; 
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
 }
--- src/main/java/com/vertex/VertexController.java
+++ src/main/java/com/vertex/VertexController.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
