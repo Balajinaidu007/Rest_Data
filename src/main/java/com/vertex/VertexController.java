963a3307139616e01794e44df081c07686429485
Balajinaidu007 <balaji.suvvada@skyronsoftware.com>
2026-03-17 12:46:25 +0530

    Adding CROS

--- src/main/java/com/vertex/VertexController.java
+++ src/main/java/com/vertex/VertexController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/vertex")
 public class VertexController {
 
